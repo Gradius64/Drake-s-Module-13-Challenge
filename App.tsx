845dd72d1abdd7dca60996afdc4791ccd865c96c
w3schools--test <66185683+Gradius64@users.noreply.github.com>
2024-11-11 19:06:54 -0600

    my tsx

---
+++ App.tsx
@@ -0,0 +1,14 @@
+// TODO: Integrate the WeatherContext component into the App component below.
+
+
+
+const message = 'Important Message';
+const alertType = "danger"
+const adminMessage = 'System Maintenance Due Soon!';
+
+const message2 = 'Important Warning';
+const alertType2 = "warning"
+const adminMessage2 = 'Database Backup Due Soon!';
+
+
+
